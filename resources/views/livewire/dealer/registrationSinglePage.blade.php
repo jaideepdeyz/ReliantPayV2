f580a5a5790acd05236758ea53f6f2e58839e723
yangeruzi <yangeruzi@gmail.com>
2023-10-20 08:12:11 +0530

    made changes

--- resources/views/livewire/dealer/registrationSinglePage.blade.php
+++ resources/views/livewire/dealer/registrationSinglePage.blade.php
@@ -1,109 +1,74 @@
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <div class="page-title-box">
-                <div class="page-title-right">
-                    <ol class="breadcrumb m-0">
-                        <li class="breadcrumb-item"><button
-                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                                class="btn btn-primary btn-sm">Logout</button>
-                        </li>
-                    </ol>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: one;">
-                        {{ csrf_field() }}
-                    </form>
-                </div>
-                <h4 class="page-title">ReliantPay | Business Registration Form</h4>
-            </div>
-        </div>
-    </div>
-    <form wire:submit="registerDealer">
-
-         {{-- multipage form  --}}
-        <div class="row">
-            <div class="col-md-12">
-                @if($currentStep==1)
-                                
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="text-uppercase bg-light p-2 mt-0 mb-3">Step 1/5: Dealer Business Information</h5>
-                            <div class="row">
-                                <div class="mb-3 col-md-12">
-                                    <label for="business_name" class="form-label">Business Name</label>
-                                    <input type="text" class="form-control @error('business_name') is-invalid @enderror"
-                                        placeholder="Enter business name" wire:model="business_name">
-                                    @error('business_name')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-    
-                                <div class="mb-3 col-md-12">
-                                    <label for="business_address" class="form-label">Address</label>
-                                    <textarea class="form-control @error('business_address') is-invalid @enderror" rows="4"
-                                        placeholder="Enter business address" wire:model="business_address"></textarea>
-                                    @error('business_address')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-    
-                                <div class="mb-3 col-md-4">
-                                    <label for="business_website" class="form-label">Website</label>
-                                    <input type="text"
-                                        class="form-control @error('business_website') is-invalid @enderror"
-                                        placeholder="Enter business website" value ="{{ old('business_website') }}"
-                                        wire:model="business_website">
-                                    @error('business_website')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-    
-                                <div class="mb-3 col-md-4">
-                                    <label for="business_email" class="form-label">E-Mail</label>
-                                    <input type="text" class="form-control @error('business_email') is-invalid @enderror"
-                                        placeholder="Enter business email" wire:model="business_email">
-                                    @error('business_email')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-    
-                                <div class="mb-3 col-md-4">
-                                    <label for="business_phone" class="form-label">Phone</label>
-                                    <input type="text"
-                                        class="form-control  @error('business_phone') is-invalid @enderror"
-                                        placeholder="Enter business phone" wire:model="business_phone">
-                                    @error('business_phone')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-
-                                <div class="col-md-12 mb-2">
-                                    <h5 style="opacity:0.7">Products & Services (Please select all applicable products &
-                                        services)</h5>
-                                </div>
-                                @foreach ($services as $service)
-                                    <div class="col-md-3 mb-2">
-                                        <div class="form-group">
-                                            <input type="checkbox" class="form-check-input"
-                                                wire:model="business_product_services.{{ $service->name }}">
-                                            <label for="" class="form-check-label">{{ $service->name }}</label>
-                                        </div>
-                                    </div>
-                                @endforeach
-                                
-                                <div class="mb-3 col-md-12 action-buttons d-flex justify-content-between">
-                                    <button type="button" class="btn w-sm btn-success waves-effect waves-light" wire:click="increaseStep()">Next</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div> <!-- end card -->
-
-                @endif
-
-                @if($currentStep==2)
+ {{-- <div class="row">
+            <div class="col-lg-12">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="text-uppercase bg-light p-2 mt-0 mb-3">Step 2/5: Statutory Compliances</h5>
                         <div class="row">
+                            <h5 class="text-uppercase bg-light p-2 mt-0 mb-3">Step 1/5: Dealer Business Information</h5>
+                            <div class="mb-3 col-md-12">
+                                <label for="business_name" class="form-label">Business Name</label>
+                                <input type="text" class="form-control @error('business_name') is-invalid @enderror"
+                                    placeholder="Enter business name" wire:model="business_name">
+                                @error('business_name')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="mb-3 col-md-12">
+                                <label for="business_address" class="form-label">Address</label>
+                                <textarea class="form-control @error('business_address') is-invalid @enderror" rows="4"
+                                    placeholder="Enter business address" wire:model="business_address"></textarea>
+                                @error('business_address')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="mb-3 col-md-4">
+                                <label for="business_website" class="form-label">Website</label>
+                                <input type="text"
+                                    class="form-control @error('business_website') is-invalid @enderror"
+                                    placeholder="Enter business website" value ="{{ old('business_website') }}"
+                                    wire:model="business_website">
+                                @error('business_website')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="mb-3 col-md-4">
+                                <label for="business_email" class="form-label">E-Mail</label>
+                                <input type="text" class="form-control @error('business_email') is-invalid @enderror"
+                                    placeholder="Enter business email" wire:model="business_email">
+                                @error('business_email')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="mb-3 col-md-4">
+                                <label for="business_phone" class="form-label">Phone</label>
+                                <input type="text"
+                                    class="form-control  @error('business_phone') is-invalid @enderror"
+                                    placeholder="Enter business phone" wire:model="business_phone">
+                                @error('business_phone')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="col-md-12 mb-2">
+                                <h5 style="opacity:0.7">Products & Services (Please select all applicable products &
+                                    services)</h5>
+                            </div>
+                            @foreach ($services as $service)
+                                <div class="col-md-3 mb-2">
+                                    <div class="form-group">
+                                        <input type="checkbox" class="form-check-input"
+                                            wire:model="business_product_services.{{ $service->name }}">
+                                        <label for="" class="form-check-label">{{ $service->name }}</label>
+                                    </div>
+                                </div>
+                            @endforeach
+
+                            <div class="col-md-12 mb-3 mt-3">
+                                <h5 class="text-uppercase bg-light p-2 mt-0 mb-3">Step 2/5: Statutory Compliances</h5>
+                            </div>
+
                             <div class="mb-3 col-md-6">
                                 <div class="form-check">
                                     <input type="checkbox" class="form-check-input"
@@ -123,20 +88,11 @@
                                     </label>
                                 </div>
                             </div>
-                            <div class="mb-3 col-md-12 action-buttons d-flex justify-content-between">
-                                <button type="button" class="btn w-sm btn-secondary waves-effect waves-light" wire:click="decreaseStep()">Previous</button>
-                                <button type="button" class="btn w-sm btn-success waves-effect waves-light" wire:click="increaseStep()">Next</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                @endif
 
-                @if($currentStep==3)
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="text-uppercase bg-light p-2 mt-0 mb-3">Step 3/5: Bank Details</h5>
-                        <div class="row">
+                            <div class="col-md-12 mb-3">
+                                <h5 class="text-uppercase mt-0 mb-3 bg-light p-2">Step 3/5: Bank Details</h5>
+                            </div>
+
                             <div class="mb-3 col-md-6">
                                 <label for="business_bank_account_name" class="form-label">Bank Account Name</label>
                                 <input type="text" class="form-control" placeholder="Enter Bank Account Name"
@@ -209,21 +165,10 @@
                                         {{ $message }}
                                     @enderror </span>
                             </div>
-
-                            <div class="mb-3 col-md-12 action-buttons d-flex justify-content-between">
-                                <button type="button" class="btn w-sm btn-secondary waves-effect waves-light" wire:click="decreaseStep()">Previous</button>
-                                <button type="button" class="btn w-sm btn-success waves-effect waves-light" wire:click="increaseStep()">Next</button>
+                            <div class="col-md-12 mb-3">
+                                <h5 class="text-uppercase mt-0 mb-3 bg-light p-2">Step 4/5: Upload Documents</h5>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                @endif
 
-                @if($currentStep==4)
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="text-uppercase bg-light p-2 mt-0 mb-3">Step 4/5: Upload Documents</h5>
-                        <div class="row">
                             <div class="mb-3 col-md-6">
                                 <label for="business_scan_signed_contract" class="form-label">Scanned Signed
                                     Contract</label>
@@ -317,20 +262,10 @@
                                     @enderror </span>
                             </div>
 
-                            <div class="mb-3 col-md-12 action-buttons d-flex justify-content-between">
-                                <button type="button" class="btn w-sm btn-secondary waves-effect waves-light" wire:click="decreaseStep()">Previous</button>
-                                <button type="button" class="btn w-sm btn-success waves-effect waves-light" wire:click="increaseStep()">Next</button>
+                            <div class="col-md-12 mb-3">
+                                <h5 class="text-uppercase bg-light p-2 mt-0 mb-3">Step 5/5: Terms & Conditions</h5>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                @endif
 
-                @if($currentStep==5)
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="text-uppercase bg-light p-2 mt-0 mb-3">Step 5/5: Terms & Conditions</h5>
-                        <div class="row">
                             <div class="mb-3 col-md-6">
                                 <div class="form-check">
                                     <input type="checkbox" class="form-check-input" id="terms" name="terms"
@@ -342,16 +277,15 @@
                                     </label>
                                 </div>
                             </div>
-                            <div class="mb-3 col-md-12 action-buttons d-flex justify-content-between">
-                                <button type="button" class="btn w-sm btn-secondary waves-effect waves-light" wire:click="decreaseStep()">Previous</button>
-                                <button type="submit" class="btn w-sm btn-success waves-effect waves-light">Submit</button>
+
+                            <div class="col-md-12 float-right">
+                                <button type="submit" class="btn w-sm btn-success waves-effect waves-light">Submit
+                                    Registration Request</button>
                             </div>
                         </div>
-                    </div>
-                </div>
-                @endif
-            </div>
-        </div>
 
-    </form>
-</div>
+                    </div>
+                </div> <!-- end card -->
+            </div> <!-- end col -->
+
+        </div> --}}
