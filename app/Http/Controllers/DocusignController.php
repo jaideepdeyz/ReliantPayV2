38dbefc848ef1eebcf30c99d14b48c89d9a4f5b1
mozhulungdsuo <mozhui.lungdsuo@gmail.com>
2023-10-26 14:48:27 +0530

    made chnages

--- app/Http/Controllers/DocusignController.php
+++ app/Http/Controllers/DocusignController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AuthorizationForm;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use DocuSign\eSign\Configuration;
@@ -13,10 +14,10 @@ use Session;
 
 
 class DocusignController extends Controller
-{   
-    
-     /** hold config value */
-     private $config;
+{
+
+    /** hold config value */
+    private $config;
 
     private $signer_client_id = 1000; # Used to indicate that the signer will use embedded
 
@@ -29,11 +30,18 @@ class DocusignController extends Controller
      *
      * @return render
      */
-    public function index()
+    public function index(Request $request)
     {
-       
+
+        // check if path has params
+        // if ($request->has('event')) {
+        //     $envelopeApi = new \DocuSign\eSign\Api\EnvelopesApi($this->apiClient());
+
+        //     $tmp_file = $envelopeApi->getDocument('203a63cf-c0e2-4dc1-b1d7-7a8d05fa5c9a', '2', 'ab884e85-a95c-48a5-a10e-d50fcb43d10b');
+        //     file_put_contents('doc/signedfile.pdf', file_get_contents($tmp_file->getPathname()));
+        // }
+
         return view('docusign.connect');
-        
     }
 
 
@@ -44,7 +52,7 @@ class DocusignController extends Controller
      */
     public function connectDocusign()
     {
-       
+
         try {
             $params = [
                 'response_type' => 'code',
@@ -102,62 +110,68 @@ class DocusignController extends Controller
         curl_close($ch);
         $decodedData = json_decode($result);
         $request->session()->put('docusign_auth_code', $decodedData->access_token);
-    
+
         return redirect()->route('docusign')->with('success', 'Docusign Succesfully Connected');
     }
 
     public function signDocument()
-    {       
-        try{
+    {
+        try {
             $this->args = $this->getTemplateArgs();
 
-        $args = $this->args;
-       
+            $args = $this->args;
 
-        $envelope_args = $args["envelope_args"];
-        
-        # Create the envelope request object
-        $envelope_definition = $this->make_envelope($args["envelope_args"]);
-        $envelope_api = $this->getEnvelopeApi();
-        # Call Envelopes::create API method
-        # Exceptions will be caught by the calling function
 
-        $api_client = new \DocuSign\eSign\client\ApiClient($this->config);
-        $envelope_api = new \DocuSign\eSign\Api\EnvelopesApi($api_client);
-        $results = $envelope_api->createEnvelope($args['account_id'], $envelope_definition);
-        $envelope_id = $results->getEnvelopeId();
+            $envelope_args = $args["envelope_args"];
 
-        $authentication_method = 'None'; # How is this application authenticating
-        # the signer? See the `authenticationMethod' definition
-        # https://developers.docusign.com/esign-rest-api/reference/Envelopes/EnvelopeViews/createRecipient
-        $recipient_view_request = new \DocuSign\eSign\Model\RecipientViewRequest([
-            'authentication_method' => $authentication_method,
-            'client_user_id' => $envelope_args['signer_client_id'],
-            'recipient_id' => '1',
-            'return_url' => $envelope_args['ds_return_url'],
-            'user_name' => 'shaiv', 'email' => 'shaivroy1@gmail.com'
-        ]);
+            # Create the envelope request object
+            $envelope_definition = $this->make_envelope($args["envelope_args"]);
+            $envelope_api = $this->getEnvelopeApi();
+            # Call Envelopes::create API method
+            # Exceptions will be caught by the calling function
 
-        $results = $envelope_api->createRecipientView($args['account_id'], $envelope_id,$recipient_view_request);
+            $api_client = new \DocuSign\eSign\client\ApiClient($this->config);
+            $envelope_api = new \DocuSign\eSign\Api\EnvelopesApi($api_client);
+            $results = $envelope_api->createEnvelope($args['account_id'], $envelope_definition);
+            $envelope_id = $results->getEnvelopeId();
 
-        return redirect()->to($results['url']);
+            $authentication_method = 'None'; # How is this application authenticating
+            # the signer? See the `authenticationMethod' definition
+            # https://developers.docusign.com/esign-rest-api/reference/Envelopes/EnvelopeViews/createRecipient
+            $recipient_view_request = new \DocuSign\eSign\Model\RecipientViewRequest([
+                'authentication_method' => $authentication_method,
+                'client_user_id' => $envelope_args['signer_client_id'],
+                'recipient_id' => '2',
+                'return_url' => $envelope_args['ds_return_url'],
+                'user_name' => 'shaiv', 'email' => 'mozhui.lungdsuo@gmail.com'
+            ]);
+
+            $results = $envelope_api->createRecipientView($args['account_id'], $envelope_id, $recipient_view_request);
+            // save envelope id and other details in database
+            AuthorizationForm::create([
+                'unsigned_document' => 'dummy.pdf',
+                'envelope_id' => $envelope_id,
+                'account_id' => $args['account_id'],
+                'document_id' => 2,
+                'ds_access_token' => $args['ds_access_token']
+            ]);
+
+
+
+            return redirect()->to($results['url']);
         } catch (Exception $e) {
             dd($e);
         }
-        
     }
 
 
     private function make_envelope($args)
-    {   
-        Log::info('make_envelope');
-
-        
+    {
         $filename = 'dummy.pdf';
 
-        $demo_docs_path = asset('doc/'.$filename);
-        Log::info($demo_docs_path);
-      
+        $demo_docs_path = asset('doc/' . $filename);
+
+
 
         // $arrContextOptions=array(
         //     "ssl"=>array(
@@ -168,28 +182,28 @@ class DocusignController extends Controller
         // set_time_limit(0);
         // $content_bytes = file_get_contents($demo_docs_path,false, stream_context_create($arrContextOptions));
         $pdf = Pdf::loadView('pdf.sample');
-       
-      
+
+
         $base64_file_content = base64_encode($pdf->output());
-        Log::info('base64_file_content');
-      
+
+
         # Create the document model
-        $document = new \DocuSign\eSign\Model\Document([# create the DocuSign document object
-        'document_base64' => $base64_file_content,
+        $document = new \DocuSign\eSign\Model\Document([ # create the DocuSign document object
+            'document_base64' => $base64_file_content,
             'name' => 'Example document', # can be different from actual file name
             'file_extension' => 'pdf', # many different document types are accepted
-            'document_id' => 1, # a label used to reference the doc
+            'document_id' => 2, # a label used to reference the doc
         ]);
         # Create the signer recipient model
-        $signer = new \DocuSign\eSign\Model\Signer([# The signer
-        'email' => 'shaivroy1@gmail.com', 'name' => 'shaiv',
-            'recipient_id' => "1", 'routing_order' => "1",
+        $signer = new \DocuSign\eSign\Model\Signer([ # The signer
+            'email' => 'mozhui.lungdsuo@gmail.com', 'name' => 'shaiv',
+            'recipient_id' => "2", 'routing_order' => "1",
             # Setting the client_user_id marks the signer as embedded
             'client_user_id' => $args['signer_client_id'],
         ]);
         # Create a sign_here tab (field on the document)
-        $sign_here = new \DocuSign\eSign\Model\SignHere([# DocuSign SignHere field/tab
-        'anchor_string' => '/sn1/', 'anchor_units' => 'pixels',
+        $sign_here = new \DocuSign\eSign\Model\SignHere([ # DocuSign SignHere field/tab
+            'anchor_string' => '/sn1/', 'anchor_units' => 'pixels',
             'anchor_y_offset' => '10', 'anchor_x_offset' => '20',
         ]);
         # Add the tabs model (including the sign_here tab) to the signer
@@ -212,10 +226,10 @@ class DocusignController extends Controller
      * Getter for the EnvelopesApi
      */
     public function getEnvelopeApi(): EnvelopesApi
-    {   
+    {
         $this->config = new Configuration();
         $this->config->setHost($this->args['base_path']);
-        $this->config->addDefaultHeader('Authorization', 'Bearer ' . $this->args['ds_access_token']);    
+        $this->config->addDefaultHeader('Authorization', 'Bearer ' . $this->args['ds_access_token']);
         $this->apiClient = new ApiClient($this->config);
 
         return new EnvelopesApi($this->apiClient);
@@ -227,8 +241,8 @@ class DocusignController extends Controller
      * @return array
      */
     private function getTemplateArgs()
-    {   
-        
+    {
+
         $envelope_args = [
             'signer_client_id' => $this->signer_client_id,
             'ds_return_url' => route('docusign')
@@ -239,8 +253,15 @@ class DocusignController extends Controller
             'ds_access_token' => Session::get('docusign_auth_code'),
             'envelope_args' => $envelope_args
         ];
-       
+
         return $args;
-        
+    }
+    private function apiClient()
+    {
+        $config = new Configuration();
+        $config->setHost(env('DOCUSIGN_BASE_URL'));
+        $config->addDefaultHeader('Authorization', 'Bearer ' . 'eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwAAZJH3_dXbSAgAAKS0BUHW20gCADPcsSF66INGpetXK3w8STYVAAEAAAAYAAEAAAAFAAAADQAkAAAAMWU5NzJjYTktZGE3OC00ODAwLThhNzMtMzI4YzQ1ZDI0YTQ4IgAkAAAAMWU5NzJjYTktZGE3OC00ODAwLThhNzMtMzI4YzQ1ZDI0YTQ4MAAAZJSz6dXbSDcAxb3Z3P_Z0Eyw_x2gfxAypw.Swr7NcBwcIjFzRyxSbsAuzjYoe63XunYibUAB_yFJdibqtLIi8P5iBLIIpLvE6EPg0KzCnoU93ii2-DthICn3MDAt35NuV_JIFLSVDAU_VIEp5LMP8INq9lx-tKUoeZIW4CwJE0_U8PViABsdWa2igM0Kt9djEvjeZCmDNJa2ji2UH0Nw_BSsKU-kMHfJgMREsduNP9FjBwnE1bcKBxrg14k6Ocd20Bw__UxfpTmchXapYrB7_nd88qPFV-qJBkzSATdO8oVxMNeblr4tOUgBaxguG5ZPW7GhXovczwCoAUR8YyfAuC3UfssF44aOXHIv1I-qs6CfetyhTj-6Qzf-A');
+        $apiClient = new ApiClient($config);
+        return $apiClient;
     }
 }
---
+++ database/migrations/2023_10_26_071435_create_authorization_forms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('authorization_forms', function (Blueprint $table) {
+            $table->id();
+            // $table->foreignId('app_id')->constrained('sale_bookings');
+            $table->string('unsigned_document')->nullable();
+            $table->string('signed_document')->nullable();
+            $table->string('envelope_id')->nullable();
+            $table->string('account_id')->nullable();
+            $table->string('document_id')->nullable();
+            $table->longText('ds_access_token')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('authorization_forms');
+    }
+};
