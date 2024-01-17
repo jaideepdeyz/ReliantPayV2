0ac64140deed67d10afcfb4d9b1c6040c0cb693c
yangeruzi <yangeruzi@gmail.com>
2024-01-17 23:45:55 +0530

    ade

--- resources/views/livewire/dealer/registration/document-uploads.blade.php
+++ resources/views/livewire/dealer/registration/document-uploads.blade.php
@@ -4,10 +4,23 @@
             <h5 class="text-uppercase bg-light p-2 mt-0 mb-3">Step 4/5: Upload Documents</h5>
             <form wire:submit.prevent="save">
                 <div class="row">
-                    <div class="mb-3 col-md-12 bg-secondary">
-                        <h5 class="text-white">All Documents to be uploaded in PDF format only with a maximum file size of 5 MB per document.</h5>
+
+                    {{-- Business EIN --}}
+                    <div class="mb-3 col-md-6" style="position:relative" x-data="{ uploading: false, progress: 0 }" x-on:livewire-upload-start="uploading = true" x-on:livewire-upload-finish="uploading = false" x-on:livewire-upload-error="uploading = false" x-on:livewire-upload-progress="progress = $event.detail.progress">
+                        <label for="business_scan_EIN" class="form-label">Scanned EIN <span class="text-danger"><sup>*</sup></span></label>
+                        <input type="file" id="business_scan_EIN" name="business_scan_EIN"
+                            class="form-control" wire:model="business_scan_EIN">
+                        <span class="text-danger"> @error('business_scan_EIN')
+                                {{ $message }}
+                            @enderror </span>
+                        <div class="mt-1" x-show="uploading"
+                        style="position:absolute;bottom:-19px;width:96%">
+                        <progress max="100" x-bind:value="progress"></progress>
+                        </div>
                     </div>
-                    <div class="mb-3 col-md-6">
+
+                    {{-- Business Contract  --}}
+                    <div class="mb-3 col-md-6" style="position:relative" x-data="{ uploading: false, progress: 0 }" x-on:livewire-upload-start="uploading = true" x-on:livewire-upload-finish="uploading = false" x-on:livewire-upload-error="uploading = false" x-on:livewire-upload-progress="progress = $event.detail.progress">
                         <label for="business_scan_signed_contract" class="form-label">Scanned Signed
                             Contract <span class="text-danger"><sup>*</sup></span></label>
                         <input type="file"
@@ -16,47 +29,50 @@
                         <span class="text-danger"> @error('business_scan_signed_contract')
                                 {{ $message }}
                             @enderror </span>
+                        <div class="mt-1" x-show="uploading"
+                            style="position:absolute;bottom:-19px;width:96%">
+                            <progress max="100" x-bind:value="progress"></progress>
+                        </div>
                     </div>
 
+                    {{-- Business PAN  --}}
                     <div class="mb-3 col-md-6">
-                        <label for="business_scan_EIN" class="form-label">Scanned EIN <span class="text-danger"><sup>*</sup></span></label>
-                        <input type="file" id="business_scan_EIN" name="business_scan_EIN"
-                            class="form-control" wire:model="business_scan_EIN">
-                        <span class="text-danger"> @error('business_scan_EIN')
-                                {{ $message }}
-                            @enderror </span>
-                    </div>
-
-                    <div class="mb-3 col-md-6">
-                        <label for="business_scan_PAN" class="form-label">Scanned PAN <span class="text-danger"><sup>*</sup></span></label>
+                        <label for="business_scan_PAN"  style="position:relative" x-data="{ uploading: false, progress: 0 }" x-on:livewire-upload-start="uploading = true" x-on:livewire-upload-finish="uploading = false" x-on:livewire-upload-error="uploading = false" x-on:livewire-upload-progress="progress = $event.detail.progress">
+                        <label for="business_scan_PAN"class="form-label">Scanned PAN <span class="text-danger"><sup>*</sup></span></label>
                         <input type="file" id="business_scan_PAN" name="business_scan_PAN"
                             class="form-control" wire:model="business_scan_PAN">
                         <span class="text-danger"> @error('business_scan_PAN')
                                 {{ $message }}
                             @enderror </span>
+                        <div class="mt-1" x-show="uploading"
+                        style="position:absolute;bottom:-19px;width:96%">
+                        <progress max="100" x-bind:value="progress"></progress>
+                        </div>
                     </div>
 
-                    <div class="mb-3 col-md-6">
-                        <label for="business_scan_registration_document" class="form-label">Scanned Business
-                            Registration <span class="text-danger"><sup>*</sup></span></label>
-                        <input type="file" id="business_scan_registration_document"
-                            name="business_scan_registration_document" class="form-control"
-                            wire:model="business_scan_registration_document">
-                        <span class="text-danger"> @error('business_scan_registration_document')
-                                {{ $message }}
-                            @enderror </span>
-                    </div>
+                    {{-- Business Bank Statement  --}}
+                    <div class="mb-3 col-md-6" style="position:relative" x-data="{ uploading: false, progress: 0 }"
+                            x-on:livewire-upload-start="uploading = true"
+                            x-on:livewire-upload-finish="uploading = false"
+                            x-on:livewire-upload-error="uploading = false"
+                            x-on:livewire-upload-progress="progress = $event.detail.progress">
+                            <label for="business_scan_bank_statement" class="form-label">Scanned Bank
+                                Statement
+                                (Last 06 Months) <span class="text-danger"><sup>*</sup></span></label>
+                            <input type="file" class="form-control"
+                                wire:model="business_scan_bank_statement">
+                            <span class="text-danger"> @error('business_scan_bank_statement')
+                                    {{ $message }}
+                                @enderror </span>
+                            <div class="mt-1" x-show="uploading"
+                                style="position:absolute;bottom:-19px;width:96%">
+                                <progress max="100" x-bind:value="progress"></progress>
+                            </div>
+                        </div>
 
-                    <div class="mb-3 col-md-6">
-                        <label for="business_scan_bank_statement" class="form-label">Scanned Bank Statement
-                            (Last 06 Months) <span class="text-danger"><sup>*</sup></span></label>
-                        <input type="file" class="form-control" wire:model="business_scan_bank_statement">
-                        <span class="text-danger"> @error('business_scan_bank_statement')
-                                {{ $message }}
-                            @enderror </span>
-                    </div>
+                    {{-- Business Utility Bills  --}}
 
-                    <div class="mb-3 col-md-6">
+                    <div class="mb-3 col-md-6" style="position:relative" x-data="{ uploading: false, progress: 0 }" x-on:livewire-upload-start="uploading = true" x-on:livewire-upload-finish="uploading = false" x-on:livewire-upload-error="uploading = false" x-on:livewire-upload-progress="progress = $event.detail.progress">
                         <label for="business_scan_utility_bills" class="form-label">Scanned Utility Bills
                             (Last 03 Months)  <span class="text-danger"><sup>*</sup></span></label>
                         <input type="file" id="business_scan_utility_bills"
@@ -65,20 +81,55 @@
                         <span class="text-danger"> @error('business_scan_utility_bills')
                                 {{ $message }}
                             @enderror </span>
+                        <div class="mt-1" x-show="uploading"
+                        style="position:absolute;bottom:-19px;width:96%">
+                        <progress max="100" x-bind:value="progress"></progress>
+                        </div>
                     </div>
 
-                    <div class="mb-3 col-md-6">
-                        <label for="business_scan_business_tax_returns" class="form-label">Business Tax
-                            Returns (Last 03 Years) <span class="text-danger"><sup>*</sup></span></label>
-                        <input type="file" id="business_scan_business_tax_returns"
-                            name="business_scan_business_tax_returns" class="form-control"
-                            wire:model="business_scan_business_tax_returns">
-                        <span class="text-danger"> @error('business_scan_business_tax_returns')
+                        {{-- Business Registration Document  --}}
+                    <div class="mb-3 col-md-6" style="position:relative" x-data="{ uploading: false, progress: 0 }"
+                        x-on:livewire-upload-start="uploading = true"
+                        x-on:livewire-upload-finish="uploading = false"
+                        x-on:livewire-upload-error="uploading = false"
+                        x-on:livewire-upload-progress="progress = $event.detail.progress">
+                        <label for="business_scan_registration_document" class="form-label">Scanned
+                            Business
+                            Registration <span class="text-danger"><sup>*</sup></span></label>
+                        <input type="file" id="business_scan_registration_document"
+                            name="business_scan_registration_document" class="form-control"
+                            wire:model="business_scan_registration_document">
+                        <span class="text-danger"> @error('business_scan_registration_document')
                                 {{ $message }}
                             @enderror </span>
+                        <div class="mt-1" x-show="uploading"
+                            style="position:absolute;bottom:-19px;width:96%">
+                            <progress max="100" x-bind:value="progress"></progress>
+                        </div>
                     </div>
 
-                    <div class="mb-3 col-md-6">
+                    <div class="mb-3 col-md-6" style="position:relative" x-data="{ uploading: false, progress: 0 }"
+                    x-on:livewire-upload-start="uploading = true"
+                    x-on:livewire-upload-finish="uploading = false"
+                    x-on:livewire-upload-error="uploading = false"
+                    x-on:livewire-upload-progress="progress = $event.detail.progress">
+                    <label for="business_scan_business_tax_returns" class="form-label">Business Tax
+                        Returns (Last 03 Years) <span class="text-danger"><sup>*</sup></span></label>
+                    <input type="file" id="business_scan_business_tax_returns"
+                        name="business_scan_business_tax_returns" class="form-control"
+                        wire:model="business_scan_business_tax_returns">
+                    <span class="text-danger"> @error('business_scan_business_tax_returns')
+                            {{ $message }}
+                        @enderror </span>
+                    <div class="mt-1" x-show="uploading"
+                        style="position:absolute;bottom:-19px;width:96%">
+                        <progress max="100" x-bind:value="progress"></progress>
+                    </div>
+                </div>
+
+                    {{-- Business External Pics --}}
+
+                    <div class="mb-3 col-md-6" style="position:relative" x-data="{ uploading: false, progress: 0 }" x-on:livewire-upload-start="uploading = true" x-on:livewire-upload-finish="uploading = false" x-on:livewire-upload-error="uploading = false" x-on:livewire-upload-progress="progress = $event.detail.progress">
                         <label for="business_premises_external_pictures" class="form-label">Business Premises
                             Pictures (External) <span class="text-danger"><sup>*</sup></span></label>
                         <input type="file" id="business_premises_external_pictures"
@@ -87,22 +138,36 @@
                         <span class="text-danger"> @error('business_premises_external_pictures')
                                 {{ $message }}
                             @enderror </span>
+                            <div class="mt-1" x-show="uploading"
+                        style="position:absolute;bottom:-19px;width:96%">
+                        <progress max="100" x-bind:value="progress"></progress>
+                        </div>
                     </div>
 
-                    <div class="mb-3 col-md-6">
-                        <label for="business_premises_internal_pictures" class="form-label">Business Premises
-                            Pictures (Office) <span class="text-danger"><sup>*</sup></span></label>
-                        <input type="file" class="form-control"
-                            wire:model="business_premises_internal_pictures">
-                        <span class="text-danger"> @error('business_premises_internal_pictures')
-                                {{ $message }}
-                            @enderror </span>
+                    <div class="mb-3 col-md-6" style="position:relative" x-data="{ uploading: false, progress: 0 }"
+                    x-on:livewire-upload-start="uploading = true"
+                    x-on:livewire-upload-finish="uploading = false"
+                    x-on:livewire-upload-error="uploading = false"
+                    x-on:livewire-upload-progress="progress = $event.detail.progress">
+                    <label for="business_premises_internal_pictures" class="form-label">Business
+                        Premises
+                        Pictures (Office) <span class="text-danger"><sup>*</sup></span></label>
+                    <input type="file" id="business_premises_internal_pictures"
+                        name="business_premises_internal_pictures" class="form-control"
+                        wire:model="business_premises_internal_pictures">
+                    <span class="text-danger"> @error('business_premises_internal_pictures')
+                            {{ $message }}
+                        @enderror </span>
+                    <div class="mt-1" x-show="uploading"
+                        style="position:absolute;bottom:-19px;width:96%">
+                        <progress max="100" x-bind:value="progress"></progress>
                     </div>
+                </div>
 
-                    <div class="mb-3 col-md-12 action-buttons d-flex justify-content-between">
-                        <button type="button" class="btn w-sm btn-secondary waves-effect waves-light" wire:click="decreaseStep">Previous</button>
-                        <button type="submit" class="btn w-sm btn-success waves-effect waves-light">Next</button>
-                    </div>
+                <div class="mb-3 col-md-12 action-buttons d-flex justify-content-between">
+                    <button type="button" class="btn btn-secondary waves-effect waves-light" wire:click="decreaseStep">Previous</button>
+                    <button type="submit" class="btn btn-success waves-effect waves-light">Next</button>
+                </div>
                 </div>
             </form>
 
