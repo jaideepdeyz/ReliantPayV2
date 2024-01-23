5507a3edea533d77d7ab4971ca020abb0f62bb17
yangeruzi <yangeruzi@gmail.com>
2024-01-23 21:11:28 +0530

    asd

--- resources/views/livewire/dealer/manage-customers.blade.php
+++ resources/views/livewire/dealer/manage-customers.blade.php
@@ -114,51 +114,5 @@
     <!-- end row -->
 
 
-    <!-- Insert Modal -->
-    <div wire:ignore.self class="modal fade" id="Countries" tabindex="-1" aria-labelledby="CountriesLabel"
-        aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="CountriesLabel">Create Country</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
-                        wire:click="closeModal"></button>
-                </div>
-
-
-                <form wire:submit.prevent="saveCountries">
-                    <div class="modal-body">
-                        <div class="mb-3">
-                            <label>Country Name</label>
-                            <input type="text" wire:model="name" class="form-control">
-                            @error('name')
-                            <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label>Country Code</label>
-                            <input type="text" wire:model="code" class="form-control">
-                            @error('code')
-                            <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                        </div>
-
-                        <div class="mb-3">
-                            <label>Flag icon</label>
-                            <input type="text" wire:model="flagimage" class="form-control">
-                            @error('flagimage')
-                            <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                        </div>
-
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" wire:click="closeModal"
-                            data-bs-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary">Save</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
+    
 </div>
