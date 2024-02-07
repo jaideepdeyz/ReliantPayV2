0b7110b8b4ecbf54051663b46106ad77d50b05ff
yangeruzi <yangeruzi@gmail.com>
2024-02-07 16:00:53 +0530

    made changes

--- database/seeders/AffiliateSeeder.php
+++ database/seeders/AffiliateSeeder.php
@@ -15,6 +15,15 @@ class AffiliateSeeder extends Seeder
     public function run(): void
     {
         Affiliate::create([
+            'user_id' => 1,
+            'affiliate_name' => 'ADMIN',
+            'affiliate_email' => 'admin@g.com',
+            'affiliate_phone' => '1234567890',
+            'is_active' => 1,
+            'affiliate_code' => 'AFL1',
+        ]);
+
+        Affiliate::create([
             'user_id' => 2,
             'affiliate_name' => 'ORGANIC',
             'affiliate_email' => 'organic@g.com',
