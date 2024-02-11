63ad57a2f69e40bac9935f197b6a8628e5c02fbc
yangeruzi <yangeruzi@gmail.com>
2024-02-11 13:03:15 +0530

    made changes

--- resources/views/livewire/modals/add-merchant-by-admin.blade.php
+++ resources/views/livewire/modals/add-merchant-by-admin.blade.php
@@ -6,27 +6,27 @@
     <div class="modal-body">
         <h6>Please Provide the details of the Merchant to create Login</h6>
         <p>
-            <small>Merchant Added will be automatically approved</small>
+            <small>Merchant Added will be automatically approved. The User Login credentials using the details that is provided below.</small>
         </p>
         <form wire:submit.prevent="createUser">
             <hr>
             <div class="row mt-3">
                 <div class="col-md-12 form-group mb-2">
-                    <label for="name">User Name</label>
+                    <label for="name">Business Owner's Name</label>
                     <input type="text" class="form-control @error('is-invalid') name @enderror" wire:model="name">
                     @error('name')
                         <span class="text-danger">{{ $message }}</span>
                     @enderror
                 </div>
                 <div class="col-md-12 form-group mb-2">
-                    <label for="email">User Email</label>
+                    <label for="email">Business Owner's Email</label>
                     <input type="email" class="form-control  @error('is-invalid') email @enderror"  wire:model.live="email">
                     @error('email')
                         <span class="text-danger">{{ $message }}</span>
                     @enderror
                 </div>
                 <div class="col-md-12 form-group mb-2">
-                    <label for="mobile">User Mobile</label>
+                    <label for="mobile">Business Owner's Mobile</label>
                     <input type="text" class="form-control @error('is-invalid') mobile @enderror"  wire:model.live="mobile" minlength="10" maxlength="10">
                     @error('mobile')
                         <span class="text-danger">{{ $message }}</span>
