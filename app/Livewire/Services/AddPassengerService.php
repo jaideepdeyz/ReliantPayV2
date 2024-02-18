23ef3d03eddb839e9c390770a7dbf8e1eb8316cd
yangeruzi <yangeruzi@gmail.com>
2024-02-18 17:29:25 +0530

    made

--- app/Livewire/Services/AddPassengerService.php
+++ app/Livewire/Services/AddPassengerService.php
@@ -64,6 +64,7 @@ class AddPassengerService extends Component
                 'requires_assistance' => $this->requires_assistance,
             ]);
             DB::commit();
+            $this->reset(['full_name','gender', 'dob', 'is_disabled', 'disability_type', 'requires_assistance']);
             $this->dispatch('message', heading:'success',text:'Passenger added');
             $this->reset(['full_name','gender', 'dob']);
         } catch (\Exception $e) {
