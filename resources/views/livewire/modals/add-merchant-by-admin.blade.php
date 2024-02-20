8cd4367a2b9e452fc9f92fba2b62d786a150c386
Jaideep Dey <jaideep.deyz@gmail.com>
2024-02-20 21:18:55 +0530

    adim merchant creation modal

--- resources/views/livewire/modals/add-merchant-by-admin.blade.php
+++ resources/views/livewire/modals/add-merchant-by-admin.blade.php
@@ -1,45 +1,51 @@
 <div>
     <div class="modal-header" wire:ignore:self>
-        <h5 class="modal-title">Add Merchant</h5>
+        <h4 class="modal-title">Add New Merchant</h4>
         <button type="button" class="btn-close" wire:click="$dispatch('hideModal')" aria-label="Close"></button>
     </div>
     <div class="modal-body">
-        <h6>Please Provide the details of the Merchant to create Login</h6>
-        <p>
-            <small>Merchant Added will be automatically approved. The User Login credentials using the details that is
-                provided below.</small>
+        {{-- <h4>Please Provide the details of the Merchant to create Login</h4> --}}
+        <p class="text-info">
+
+                Merchants added by the administrator will be automatically approved. The login credentails will be sent to the email provided below.
+
         </p>
         <form wire:submit.prevent="createUser">
-            <hr>
-            <div class="row mt-3">
+
+            <div class="row mb-3">
                 <div class="col-md-12 form-group mb-2">
                     <label for="name">Business Owner's Name <span class="text-danger"><sup>*</sup></span></label>
-                    <input type="text" class="form-control @error('is-invalid') name @enderror" wire:model="name">
+                    <input type="text" style="color: #089000;" class="form-control @error('is-invalid') name @enderror" wire:model="name">
                     @error('name')
                     <span class="text-danger">{{ $message }}</span>
                     @enderror
                 </div>
                 <div class="col-md-12 form-group mb-2">
                     <label for="email">Business Owner's Email <span class="text-danger"><sup>*</sup></span></label>
-                    <input type="email" class="form-control  @error('is-invalid') email @enderror" wire:model="email">
+                    <input type="email" style="color: #089000;" class="form-control  @error('is-invalid') email @enderror" wire:model="email">
                     @error('email')
                     <span class="text-danger">{{ $message }}</span>
                     @enderror
                 </div>
                 <div class="col-md-12 form-group mb-2">
                     <label for="mobile">Business Owner's Mobile <span class="text-danger"><sup>*</sup></span></label>
-                    <input type="text" class="form-control @error('is-invalid') mobile @enderror" wire:model="mobile"
+                    <input type="text" style="color: #089000;" class="form-control @error('is-invalid') mobile @enderror" wire:model="mobile"
                         minlength="10" maxlength="10" placeholder="Enter the 10 digit mobile number without dashes">
                     @error('mobile')
                     <span class="text-danger">{{ $message }}</span>
                     @enderror
                 </div>
+
+            </div>
+            <div class="row mb-3 float-right">
                 <div class="col-md-12 mb-2 mt-2">
-                    <button type="submit" class="btn btn-primary" wire:loading.attr="disabled"
+                    <button type="submit" class="btn btn-info" wire:loading.attr="disabled"
                         wire:loading.class='"disabled'>
-                        <span class="spinner-border spinner-border-sm me-1" role="status" aria-hidden="true"
-                            wire:loading></span>
-                        Create User & Proceed
+                        <span wire:loading>
+                            <i class="fas fa-spinner fa-spin"></i>
+                                Creating Merchant
+                        </span>
+                        <span wire:loading.remove>Create Merchant</span>
                     </button>
                 </div>
             </div>
