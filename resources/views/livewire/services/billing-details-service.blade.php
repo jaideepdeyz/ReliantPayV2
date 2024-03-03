418f43001338209ca29985d1decc63ab241e224c
mozhulungdsuo <mozhui.lungdsuo@gmail.com>
2024-03-03 12:48:07 +0530

    nadec ages

--- resources/views/livewire/services/billing-details-service.blade.php
+++ resources/views/livewire/services/billing-details-service.blade.php
@@ -5,7 +5,7 @@
                 <ol class="breadcrumb m-0">
                     <li class="breadcrumb-item"><a href="javascript: void(0);">Reliant Pay</a></li>
                     <li class="breadcrumb-item">Agent Dashboard</li>
-                    <li class="breadcrumb-item">{{$saleBooking->service->service_name}}</li>
+                    <li class="breadcrumb-item">{{ $saleBooking->service->service_name }}</li>
                     <li class="breadcrumb-item active">Billing Details</li>
                 </ol>
             </div>
@@ -22,12 +22,14 @@
                         <div class="row">
 
                             <div class="mb-3 col-md-4">
-                                <label for="cc_type" class="form-label">Debit/Credit Card Type? <span class="text-danger"><sup>*</sup></span></label>
+                                <label for="cc_type" class="form-label">Debit/Credit Card Type? <span
+                                        class="text-danger"><sup>*</sup></span></label>
                                 <select class="form-control @error('cc_type') is-invalid @enderror"
                                     wire:model.live="cc_type">
                                     <option value="Mastercard" selected>Mastercard</option>
                                     <option value="VISA">VISA</option>
                                     <option value="American Express">American Express</option>
+                                    <option value="Discover">Discover</option>
 
                                 </select>
                                 @error('cc_type')
@@ -36,15 +38,15 @@
                             </div>
 
                             <div class="mb-3 col-md-4">
-                                <label for="cc_number" class="form-label">Credit Card Number <span class="text-danger"><sup>*</sup></span></label>
+                                <label for="cc_number" class="form-label">Credit Card Number <span
+                                        class="text-danger"><sup>*</sup></span></label>
 
-                                <input type="text" data-toggle="input-mask"
-                                data-mask-format={{ $cc_type=='American Express'? "0000-00000000-0000" : "0000-0000-0000-0000"}}
-                                placeholder={{ $cc_type=='American Express'? "0000-00000000-0000" : "0000-0000-0000-0000"}}
-                                class="form-control @error('cc_number') is-invalid @enderror"
-                                wire:model="cc_number"
-                                minlength={{ $cc_type=='American Express'? 17 : 19}}
-                                maxlength={{ $cc_type=='American Express'? 17 : 19}}>
+                                <input type="text" id="cc_number" {{-- data-toggle="input-mask"
+                                data-mask-format={{ $maskFormat}}
+                                placeholder={{ $maskFormat}} --}}
+                                placeholder={{ $maskFormat}}
+                                    class="form-control @error('cc_number') is-invalid @enderror"
+                                    wire:model.live="cc_number">
 
 
                                 @error('cc_number')
@@ -53,7 +55,8 @@
                             </div>
 
                             <div class="mb-3 col-md-2">
-                                <label for="cc_expiration_date" class="form-label">Card Expiry Date  <span class="text-danger"><sup>*</sup></span></label>
+                                <label for="cc_expiration_date" class="form-label">Card Expiry Date <span
+                                        class="text-danger"><sup>*</sup></span></label>
                                 <input type="text" data-toggle="input-mask" data-mask-format="00/00"
                                     class="form-control @error('cc_expiration_date') is-invalid @enderror"
                                     wire:model="cc_expiration_date" placeholder="MM/YY">
@@ -65,8 +68,10 @@
 
 
                             <div class="mb-3 col-md-2">
-                                <label for="cc_cvc" class="form-label">CVV: <span class="text-danger"><sup>*</sup></span></label>
-                                <input type="text" class="form-control @error('cc_cvc') is-invalid @enderror" wire:model="cc_cvc" minlength="3" maxlength="3">
+                                <label for="cc_cvc" class="form-label">CVV: <span
+                                        class="text-danger"><sup>*</sup></span></label>
+                                <input type="text" class="form-control @error('cc_cvc') is-invalid @enderror"
+                                    wire:model="cc_cvc" minlength="3" maxlength="3">
                                 @error('cc_cvc')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
@@ -74,7 +79,9 @@
 
                             <div class="mb-3 col-md-3">
                                 <label for="cc_billing_address_street">Street Address:</label>
-                                <input type="text" class="form-control @error('cc_billing_address_street') is-invalid @enderror mt-1" wire:model="cc_billing_address_street">
+                                <input type="text"
+                                    class="form-control @error('cc_billing_address_street') is-invalid @enderror mt-1"
+                                    wire:model="cc_billing_address_street">
                                 @error('cc_billing_address_street')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
@@ -87,9 +94,9 @@
                                     {{-- <input type="text" class="form-input form-control @error('cityQuery') is-invalid @enderror"
                                         placeholder="Search Cities .." wire:model.live="cityQuery"
                                         required> --}}
-                                    <input type="text" class="form-input form-control @error('cityQuery') is-invalid @enderror"
-                                        placeholder="Enter City" wire:model="cc_billing_address_city"
-                                        required>
+                                    <input type="text"
+                                        class="form-input form-control @error('cityQuery') is-invalid @enderror"
+                                        placeholder="Enter City" wire:model="cc_billing_address_city" required>
                                     {{-- @if ($cities)
                                     <div class="dropdown-container">
                                         @foreach ($cities as $selectedCity)
@@ -101,8 +108,8 @@
                                     </div>
                                     @endif --}}
                                     @error('cc_billing_address_city')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
                                 </div>
                             </div>
 
@@ -113,10 +120,10 @@
                                     {{-- <input type="text" class="form-input form-control @error('stateQuery') is-invalid @enderror"
                                         placeholder="Search States .." wire:model.live="stateQuery"
                                         required> --}}
-                                    <input type="text" class="form-input form-control @error('stateQuery') is-invalid @enderror"
-                                        placeholder="Enter State" wire:model="cc_billing_address_state"
-                                        required>
-                                   {{-- @if ($states)
+                                    <input type="text"
+                                        class="form-input form-control @error('stateQuery') is-invalid @enderror"
+                                        placeholder="Enter State" wire:model="cc_billing_address_state" required>
+                                    {{-- @if ($states)
                                     <div class="dropdown-container">
                                         @foreach ($states as $state)
                                         <a href="#" class="d-block dropdown-links"
@@ -136,8 +143,10 @@
                             </div>
 
                             <div class="mb-3 col-md-3">
-                                <label for="cc_billing_address_zip" class="form-label">ZIP Code <span class="text-danger"><sup>*</sup></span></label>
-                                <input type="text" class="form-control @error('cc_billing_address_zip') is-invalid @enderror"
+                                <label for="cc_billing_address_zip" class="form-label">ZIP Code <span
+                                        class="text-danger"><sup>*</sup></span></label>
+                                <input type="text"
+                                    class="form-control @error('cc_billing_address_zip') is-invalid @enderror"
                                     wire:model="cc_billing_address_zip" minlength="5" maxlength="5">
                                 @error('cc_billing_address_zip')
                                     <div class="invalid-feedback">{{ $message }}</div>
@@ -145,7 +154,8 @@
                             </div>
 
                             <div class="mb-3 col-md-6">
-                                <label for="cc_name" class="form-label">Card Holder's Name <span class="text-danger"><sup>*</sup></span></label>
+                                <label for="cc_name" class="form-label">Card Holder's Name <span
+                                        class="text-danger"><sup>*</sup></span></label>
                                 <input type="text" class="form-control @error('cc_name') is-invalid @enderror"
                                     wire:model="cc_name">
                                 @error('cc_name')
@@ -154,7 +164,8 @@
                             </div>
 
                             <div class="mb-3 col-md-6">
-                                <label for="cc_dob" class="form-label">Card Holder's Date of Birth <span class="text-danger"><sup>*</sup></span></label>
+                                <label for="cc_dob" class="form-label">Card Holder's Date of Birth <span
+                                        class="text-danger"><sup>*</sup></span></label>
                                 <input type="date" class="form-control @error('cc_dob') is-invalid @enderror"
                                     wire:model="cc_dob">
                                 @error('cc_dob')
@@ -163,16 +174,19 @@
                             </div>
 
                             <div class="mb-3 col-md-6">
-                                <label for="cc_phone" class="form-label">Card Holder's Phone <span class="text-danger"><sup>*</sup></span></label>
+                                <label for="cc_phone" class="form-label">Card Holder's Phone <span
+                                        class="text-danger"><sup>*</sup></span></label>
                                 <input type="text" class="form-control @error('cc_phone') is-invalid @enderror"
-                                    wire:model="cc_phone" minlength="10" maxlength="10" placeholder="Enter 10 digit mobile number">
+                                    wire:model="cc_phone" minlength="10" maxlength="10"
+                                    placeholder="Enter 10 digit mobile number">
                                 @error('cc_phone')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
 
                             <div class="mb-3 col-md-6">
-                                <label for="cc_email" class="form-label">Card Holder's Email <span class="text-danger"><sup>*</sup></span></label>
+                                <label for="cc_email" class="form-label">Card Holder's Email <span
+                                        class="text-danger"><sup>*</sup></span></label>
                                 <input type="text" class="form-control @error('cc_email') is-invalid @enderror"
                                     wire:model="cc_email">
                                 @error('cc_email')
@@ -181,7 +195,8 @@
                             </div>
 
                             <div class="mb-3 col-md-4">
-                                <label for="amount_charged" class="form-label">Total Amount Charged (in USD): <span class="text-danger"><sup>*</sup></span></label>
+                                <label for="amount_charged" class="form-label">Total Amount Charged (in USD): <span
+                                        class="text-danger"><sup>*</sup></span></label>
                                 <input class="form-control @error('amount_charged') is-invalid @enderror"
                                     wire:model="amount_charged" readonly>
                                 @error('amount_charged')
@@ -190,8 +205,10 @@
                             </div>
 
                             <div class="mb-3 col-md-4">
-                                <label for="bookedThroughReservationAssistance" class="form-label">Ticket booked through Reservation Assistance ?:</label>
-                                <select class="form-control @error('bookedThroughReservationAssistance') is-invalid @enderror"
+                                <label for="bookedThroughReservationAssistance" class="form-label">Ticket booked
+                                    through Reservation Assistance ?:</label>
+                                <select
+                                    class="form-control @error('bookedThroughReservationAssistance') is-invalid @enderror"
                                     wire:model="bookedThroughReservationAssistance">
                                     <option value="">Select Option</option>
                                     <option value="Yes">Yes</option>
@@ -210,8 +227,10 @@
                                 @enderror
                             </div>
 
-                            <div class="mb-3 col-md-6 @if($showIdUpload == 'No') d-none @endif">
-                                <label for="primary_passenger_id_doc" class="form-label">Primary Passenger ID <span class="text-danger">(mandatory for transactions above $500) <sup>*</sup></span> </label>
+                            <div class="mb-3 col-md-6 @if ($showIdUpload == 'No') d-none @endif">
+                                <label for="primary_passenger_id_doc" class="form-label">Primary Passenger ID <span
+                                        class="text-danger">(mandatory for transactions above $500) <sup>*</sup></span>
+                                </label>
                                 <input type="file" id="primary_passenger_id_doc" name="primary_passenger_id_doc"
                                     class="form-control" wire:model="primary_passenger_id_doc">
                                 <span class="text-danger"> @error('primary_passenger_id_doc')
@@ -231,4 +250,15 @@
             </div>
         </form>
     </div>
+    <script>
+        window.addEventListener('maskChange', event => {
+      
+            $('#cc_number').mask(event.detail[0]['mask']);
+           
+            $('#cc_number').attr('minlength', event.detail[0]['cc_length']);
+            $('#cc_number').attr('maxlength', event.detail[0]['cc_length']);
+
+
+        });
+    </script>
 </div>
