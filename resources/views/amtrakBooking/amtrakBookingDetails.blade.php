0d8d8707bc832d6723988449b4615c77172a2335
Jaideep Dey <jaideep.deyz@gmail.com>
2024-03-09 10:00:38 +0530

    added preview modal

--- resources/views/amtrakBooking/amtrakBookingDetails.blade.php
+++ resources/views/amtrakBooking/amtrakBookingDetails.blade.php
@@ -220,10 +220,10 @@
 
     <div wire:ignore.self class="modal fade" id="PreviewModal" tabindex="-1"
             aria-labelledby="ImagePreviewModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
+        <div class="modal-dialog modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Document Preview</h5>
+                    <h5 class="modal-title">Itinerary Preview</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" wire:click="closeModal"
                         aria-label="Close"></button>
                 </div>
@@ -232,7 +232,7 @@
 
                         @if ($amtrakDetails->itenary->document_filepath)
 
-                            <img src="{{Storage::URL($amtrakDetails->itenary->document_filepath)}}" alt="Itenary" width="600">
+                            <img src="{{Storage::URL($amtrakDetails->itenary->document_filepath)}}" alt="Itenary" width="1000">
 
 
 
